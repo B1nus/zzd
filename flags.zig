0b105f83872c759e962c8b97aa02eda646585a03
B1nus <123@1>
2024-10-09 01:11:12 +0200

    Flag parsing implemented

---
+++ flags.zig
@@ -0,0 +1,81 @@
+const std = @import("std");
+const cmp = std.mem.eql;
+
+const FlagError = error{
+    UnexpectedNumber,
+    ExpectedFlag,
+    InvalidFlag,
+    ExpectedColumn,
+    InvalidColumn,
+    NotInColumnRange,
+    ExpectedGroup,
+    InvalidGroup,
+    ExpectedOffset,
+    InvalidOffset,
+    ExpectedLines,
+    InvalidLines,
+};
+
+const Flags = struct {
+    revert: bool = false,
+    upper: bool = false,
+    cols: usize = 16,
+    group: usize = 2,
+    offset: usize = 0,
+    lines: ?usize = null,
+    little_endian: bool = false,
+};
+
+pub fn parse_flags(flag_args: [][:0]u8) FlagError!Flags {
+    var flags = Flags{};
+
+    var i: usize = 0;
+    while (i < flag_args.len) : (i += 1) {
+        if (flag_args[i][0] >= 48 and flag_args[i][0] <= 58) {
+            return FlagError.UnexpectedNumber;
+        }
+        if (flag_args[i][0] == '-') {
+            if (flag_args[i].len == 2) {
+                switch (flag_args[i][1]) {
+                    'r' => flags.revert = true,
+                    'u' => flags.upper = true,
+                    'e' => flags.little_endian = true,
+                    'c' => flags.cols = try parse_flag_number(flag_args, &i, FlagError.ExpectedColumn, FlagError.InvalidColumn, 1),
+                    'g' => flags.group = try parse_flag_number(flag_args, &i, FlagError.ExpectedGroup, FlagError.InvalidGroup, 1),
+                    's' => flags.offset = try parse_flag_number(flag_args, &i, FlagError.ExpectedOffset, FlagError.InvalidOffset, 0),
+                    'l' => flags.lines = try parse_flag_number(flag_args, &i, FlagError.ExpectedLines, FlagError.InvalidLines, 1),
+                    else => return FlagError.InvalidFlag,
+                }
+            } else {
+                return FlagError.InvalidFlag;
+            }
+        } else {
+            return FlagError.ExpectedFlag;
+        }
+    }
+
+    return flags;
+}
+
+pub fn parse_flag_number(flag_args: [][:0]u8, i: *usize, expected: FlagError, invalid: FlagError, min: usize) FlagError!usize {
+    if (flag_args.len >= i.* + 2) {
+        i.* += 1;
+        switch (flag_args[i.*][0]) {
+            '0'...'9' => {
+                const num = std.fmt.parseInt(usize, flag_args[i.*], 10) catch {
+                    return invalid;
+                };
+                if (num < min) {
+                    return invalid;
+                } else {
+                    return num;
+                }
+            },
+            else => {
+                return expected;
+            },
+        }
+    } else {
+        return expected;
+    }
+}
--- zzd.zig
+++ zzd.zig
@@ -1,107 +1,84 @@
 const std = @import("std");
+const flag_parser = @import("flags.zig");
+const parse_flags = flag_parser.parse_flags;
 const stdout = std.io.getStdOut().writer();
 var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-
-// TODO, let user choose chunk size and upper/lowercase
-
-const zzdError = error{
-    NoArgument,
-    TooManyArguments,
-    FileNotFound,
-    InvalidCase,
-};
 
 pub fn main() !void {
     const args = try std.process.argsAlloc(arena.allocator());
     defer std.process.argsFree(arena.allocator(), args);
 
-    var chunk_size: usize = 2;
-    var columns: usize = 8;
-    var case = std.fmt.Case.lower;
-
     // Check argument amounts
-    switch (args.len) {
-        1 => return zzdError.NoArgument,
-        2 => {},
-        3 => {
-            chunk_size = try std.fmt.parseInt(usize, args[2], 10);
-        },
-        4 => {
-            chunk_size = try std.fmt.parseInt(usize, args[2], 10);
-            columns = try std.fmt.parseInt(usize, args[3], 10);
-        },
-        5 => {
-            chunk_size = try std.fmt.parseInt(usize, args[2], 10);
-            columns = try std.fmt.parseInt(usize, args[3], 10);
-            case = switch (args[4][0]) {
-                'l' => std.fmt.Case.lower,
-                'u' => std.fmt.Case.upper,
-                else => return zzdError.InvalidCase,
-            };
-        },
-        else => return zzdError.TooManyArguments,
-    }
+    const flags = parse_flags(args[1..]);
 
-    // Get file handle
-    const path = args[1];
-    const file = std.fs.cwd().openFile(path, .{ .mode = .read_only }) catch |err| switch (err) {
-        error.FileNotFound => {
-            std.debug.print("File '{s}' not found: \n", .{path});
-            return zzdError.FileNotFound;
-        },
-        else => return err,
-    };
+    std.debug.print("{any}\n", .{flags});
 
-    // Read
-    const reader = file.reader();
-    const stat = try file.stat();
-    const buffer = try arena.allocator().alloc(u8, stat.size);
-    _ = try reader.read(buffer);
-    defer arena.allocator().free(buffer);
-    var lines = std.mem.window(u8, buffer, chunk_size * columns, chunk_size * columns);
+    return;
 
-    // const red = "\x1b[91m";
-    const bold = "\x1b[1m";
-    const green = "\x1b[32m";
-    const yellow = "\x1b[33m";
-    const reset = "\x1b[0m";
+    // // Get file handle
+    // const path = args[1];
+    // const file = std.fs.cwd().openFile(path, .{ .mode = .read_only }) catch |err| switch (err) {
+    //     error.FileNotFound => {
+    //         std.debug.print("File '{s}' not found: \n", .{path});
+    //         return zzdError.FileNotFound;
+    //     },
+    //     else => return err,
+    // };
+    //
+    // // Read
+    // const reader = file.reader();
+    // const stat = try file.stat();
+    // const buffer = try arena.allocator().alloc(u8, stat.size);
+    // _ = try reader.read(buffer);
+    // defer arena.allocator().free(buffer);
+    // var lines = std.mem.window(u8, buffer, chunk_size * columns, chunk_size * columns);
+    //
+    // // const red = "\x1b[91m";
+    // const bold = "\x1b[1m";
+    // const green = "\x1b[32m";
+    // const yellow = "\x1b[33m";
+    // const reset = "\x1b[0m";
+    //
+    // var line_i: usize = 0;
+    // while (lines.next()) |line| {
+    //     try stdout.print("{x:0>8}: {s}", .{ line_i * chunk_size * columns, bold });
+    //
+    //     var empy = false;
+    //     for (0..(chunk_size * columns)) |i| {
+    //         // Column
+    //         if (i % chunk_size == 0 and i > 0) {
+    //             try stdout.print(" ", .{});
+    //         }
+    //
+    //         if (empy) {
+    //             try stdout.print("  ", .{});
+    //         } else {
+    //             // End of file
+    //             empy = (i + line_i * chunk_size * columns + 1 >= stat.size);
+    //
+    //             const array = [1]u8{line[i]};
+    //             const as_hex = std.fmt.bytesToHex(array, case);
+    //             if (line[i] == '\n') {
+    //                 try stdout.print("{s}{s}", .{ yellow, as_hex });
+    //             } else {
+    //                 try stdout.print("{s}{s}", .{ green, as_hex });
+    //             }
+    //         }
+    //     }
+    //
+    //     try stdout.print("  ", .{});
+    //
+    //     for (line) |c| switch (c) {
+    //         '\n' => std.debug.print("{s}.", .{yellow}),
+    //         '\r' => std.debug.print("{s}.", .{green}),
+    //         '\t' => std.debug.print("{s}.", .{green}),
+    //         else => std.debug.print("{s}{c}", .{ green, c }),
+    //     };
+    //     try stdout.print("\n{s}", .{reset});
+    //     line_i += 1;
+    // }
+}
 
-    var line_i: usize = 0;
-    while (lines.next()) |line| {
-        try stdout.print("{x:0>8}: {s}", .{ line_i * chunk_size * columns, bold });
-
-        var empy = false;
-        for (0..(chunk_size * columns)) |i| {
-            // Column
-            if (i % chunk_size == 0 and i > 0) {
-                try stdout.print(" ", .{});
-            }
-
-            if (empy) {
-                try stdout.print("  ", .{});
-            } else {
-                // End of file
-                empy = (i + line_i * chunk_size * columns + 1 >= stat.size);
-
-                const array = [1]u8{line[i]};
-                const as_hex = std.fmt.bytesToHex(array, case);
-                if (line[i] == '\n') {
-                    try stdout.print("{s}{s}", .{ yellow, as_hex });
-                } else {
-                    try stdout.print("{s}{s}", .{ green, as_hex });
-                }
-            }
-        }
-
-        try stdout.print("  ", .{});
-
-        for (line) |c| switch (c) {
-            '\n' => std.debug.print("{s}.", .{yellow}),
-            '\r' => std.debug.print("{s}.", .{green}),
-            '\t' => std.debug.print("{s}.", .{green}),
-            else => std.debug.print("{s}{c}", .{ green, c }),
-        };
-        try stdout.print("\n{s}", .{reset});
-        line_i += 1;
-    }
+pub fn bytes(file: []const u8) []u8 {
+    return file;
 }
